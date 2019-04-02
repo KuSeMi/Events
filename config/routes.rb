0cd0af78bfcd8ad4fdeaec1b2b6ee4cc0fb9c0be
kusemi <skushnir@i.ua>
2019-04-02 12:01:58 +0300

    sessions cntrlr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resource :sessions
+
   get "signup" => "users#new"
   resources :users
   root "events#index"
+  
   resources :events do
     resources :registrations
   end
