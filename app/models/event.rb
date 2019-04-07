c80d125f1699d958d706b6fe59a0010deb9f52b6
kusemi <skushnir@i.ua>
2019-04-07 20:42:18 +0300

    Custon Routes

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,14 +17,23 @@ class Event < ApplicationRecord
   has_many :likers, through: :likes, source: :user
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
-  
+
+  scope :upcoming, -> { where('starts_at >= ?', Time.now).order(:starts_at) }
+  scope :past, -> { where('starts_at < ?', Time.now).order(:starts_at) }
+  scope :free, -> { upcoming.where(price: 0.0).order(:name) }
+  scope :recent, ->(max=3) { past.limit(max) }
+
   def free?
     price.blank? || price.zero?  
   end
 
-  def self.upcoming
-    where("starts_at >= ?", Time.now).order("starts_at")
-  end
+  # def self.past
+  #   where("starts_at < ?", Time.now).order("starts_at")
+  # end
+
+  # def self.upcoming
+  #   where("starts_at >= ?", Time.now).order("starts_at")
+  # end
 
   def spots_left
     if capacity.zero?
